46419d3c9e372e0e53fbb662b519b5d9c0deb06a
kidusgetagizaw <137602728+kidusgetagizaw@users.noreply.github.com>
2023-07-04 01:58:46 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,42 @@ import telebot
 import time
 import mysql.connector
 
-
-TOKEN='6323409890:AAHwUHsxAsD3zSzGazHTaR8-osC1ShJEh9E'
+mydb = mysql.connector.connect(
+  host="bk7pnsmdq1aanltzjnlj-mysql.services.clever-cloud.com",
+  user="uwjaio6g2ghrakr1",
+  password="mu5qlNLAB2ssrWMp0KZ0",
+  database="bk7pnsmdq1aanltzjnlj"
+)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM TELEGRAM")
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
+TOKEN='5912502878:AAG4UWyN8ZXvx0fzFCJSKK0hFsOHYgd9tlA'
 bot=telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start','START'])
 def start(message):
   bot.send_message(message.from_user.id,'hello user this is created by kidus geta')
-
+test=True
 @bot.message_handler(func = lambda message: True)
 def Telegram_bots(message):
-    bot.send_message(message.from_user.id,message.text)
+    global mydb,test
+    mycursor = mydb.cursor()
+    mycursor.execute("INSERT INTO TELEGRAM (id2,name,message,seen) VALUES ('"+str(message.from_user.id)+"','"+message.from_user.first_name+"','"+message.text+"','false')")
+    mydb.commit()
+    if test:
+      while True:
+        test=False
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM TELEGRAM")
+        myresult = mycursor.fetchall()
+        for x in myresult:
+          if x[4] == 'sender':
+            bot.send_message(x[1],x[3])
+            
+            mycursor = mydb.cursor()
+            mycursor.execute("UPDATE TELEGRAM SET seen = 'sended' WHERE seen = 'sender'")
+            mydb.commit()
+        time.sleep(1)
 bot.infinity_polling()
