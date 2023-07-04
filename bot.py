4b8b005d144ff4167794cc067d401706bb687605
kidusgetagizaw <137602728+kidusgetagizaw@users.noreply.github.com>
2023-07-04 01:46:43 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,1 @@
-import telebot
-import time
-import mysql.connector
-
-
-TOKEN='6323409890:AAHwUHsxAsD3zSzGazHTaR8-osC1ShJEh9E'
-bot=telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start','START'])
-def start(message):
-  bot.send_message(message.from_user.id,'hello user this is created by kidus geta')
-
-@bot.message_handler(func = lambda message: True)
-def Telegram_bots(message):
-    bot.send_message(message.from_user.id,message.text)
-bot.infinity_polling()
+print('hello world')
